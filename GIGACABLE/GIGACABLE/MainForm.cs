ce396b194b4e0b8cf298e5aeda8ff4cd22a652ba
irbiing <36492604+irbiing@users.noreply.github.com>
2019-04-07 21:21:27 -0500

    avance de interfaz

---
+++ GIGACABLE/GIGACABLE/MainForm.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: irving
+ * Date: 05/04/2019
+ * Time: 09:42 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GIGACABLE
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox2TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
